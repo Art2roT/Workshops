0d2ee61d1b8c05d72338f2a082403af581caacec
Art2roT <arturotejada2@gmail.com>
2024-09-15 12:19:18 -0400

    reset everything cause broke

--- workshop-02-typescript/practice-problems/problem-1/main.ts
+++ workshop-02-typescript/practice-problems/problem-1/main.ts
@@ -1,14 +1,1 @@
 // Fill in for exercises!
-
-let userName: string = "Arturo"
-
-let favFoods: string[] = ["Sushi", "Mac-and-cheese", "fried chicken"]
-
-let user = {
-    name: userName,
-    age: 42,
-    favFoods: favFoods
-}
-
-console.log(user)
-console.log(user.favFoods[1])
