bfd09daec9bc221aee99bd6516f0e438888cbe7d
Art2roT <arturotejada2@gmail.com>
2024-09-15 11:47:39 -0400

    Completed problem 1

---
+++ workshop-02-typescript/practice-problems/problem-1/main.js
@@ -0,0 +1,9 @@
+// Fill in for exercises!
+var userName = "Arturo";
+var favFoods = ["Sushi", "Mac-and-cheese", "fried chicken"];
+var user = {
+    name: userName,
+    age: 42,
+    favFoods: favFoods
+};
+console.log(user);
--- workshop-02-typescript/practice-problems/problem-1/main.ts
+++ workshop-02-typescript/practice-problems/problem-1/main.ts
@@ -1,1 +1,13 @@
 // Fill in for exercises!
+
+let userName: string = "Arturo"
+
+let favFoods: string[] = ["Sushi", "Mac-and-cheese", "fried chicken"]
+
+let user = {
+    name: userName,
+    age: 42,
+    favFoods: favFoods
+}
+
+console.log(user)
